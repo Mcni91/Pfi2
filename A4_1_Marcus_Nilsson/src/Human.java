f5cdb6a37a2ae80470fb7ede44dc917a3e1d1a1c
Mcni91 <mcni.91@gmail.com>
2015-02-10 16:22:21 +0100

    Human class created

---
+++ A4_1_Marcus_Nilsson/src/Human.java
@@ -0,0 +1,20 @@
+import java.util.jar.Attributes.Name;
+
+public class Human {
+	private Dog dog;
+	private String name;
+	
+	public Human(String name) {
+		this.name = name;
+	}
+	public String getName(){
+		return this.name;
+	}
+	public void buyDog(Dog dog){
+		
+	}
+	/**Returnerar: "x äger en hund som heter y" eller " x äger inte en hund"*/
+	public String getInfo(){
+		
+	}
+}
