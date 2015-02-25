8a5cba41d2e3ceb8296f111386fdb117ac8f4e20
Mcni91 <mcni.91@gmail.com>
2015-02-25 02:24:36 +0100

    4: Testa 2: check!

--- A6_Marcus_Nilsson/src/se/mah/k3lara/skaneAPI/view/TestGUI.java
+++ A6_Marcus_Nilsson/src/se/mah/k3lara/skaneAPI/view/TestGUI.java
@@ -11,12 +11,17 @@ import javax.swing.JTextArea;
 import javax.swing.BoxLayout;
 import javax.swing.JSplitPane;
 
+import se.mah.k3lara.skaneAPI.control.Constants;
+import se.mah.k3lara.skaneAPI.model.Journey;
+import se.mah.k3lara.skaneAPI.model.Journeys;
 import se.mah.k3lara.skaneAPI.model.Station;
 import se.mah.k3lara.skaneAPI.xmlparser.Parser;
 
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.util.ArrayList;
+import java.util.Calendar;
+
 import javax.swing.JScrollPane;
 
 
@@ -27,6 +32,10 @@ public class TestGUI extends JFrame {
 	private JTextField textField;
 	private JTextArea textArea;
 	private ArrayList<Station> stations;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextArea textArea_1;
+	private Journeys journeys;
 
 	/**
 	 * Launch the application.
@@ -48,7 +57,6 @@ public class TestGUI extends JFrame {
 	 * Create the frame.
 	 */
 	public TestGUI() {
-		stations = new ArrayList<Station>(); 
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 600);
@@ -76,10 +84,11 @@ public class TestGUI extends JFrame {
 		JButton btnSearch = new JButton("Search");
 		btnSearch.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				stations = new ArrayList<Station>(); 
 				stations.addAll( Parser.getStationsFromURL( textField.getText() ));
 				for (Station station : stations) {
-					textArea.append(station.getStationName() +" (" +station.getStationNbr() + ")\n");
-					textArea.append("Latitud: " + station.getLatitude() +" Longitude: " +station.getLongitude() + "\n\n");
+					textArea.append(station.getStationName() + " (" +station.getStationNbr() + ")\n");
+					textArea.append("Latitud: " + station.getLatitude() +" Longitude: " + station.getLongitude() + "\n\n");
 				}
 			}
 		});
@@ -90,6 +99,56 @@ public class TestGUI extends JFrame {
 		
 		textArea = new JTextArea();
 		scrollPane.setViewportView(textArea);
+		
+		JPanel panel_2 = new JPanel();
+		splitPane.setRightComponent(panel_2);
+		panel_2.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel_3 = new JPanel();
+		panel_2.add(panel_3, BorderLayout.NORTH);
+		panel_3.setLayout(new BoxLayout(panel_3, BoxLayout.X_AXIS));
+		
+		textField_1 = new JTextField();
+		panel_3.add(textField_1);
+		textField_1.setColumns(10);
+		
+		textField_2 = new JTextField();
+		panel_3.add(textField_2);
+		textField_2.setColumns(10);
+		
+		JButton btnSearch_1 = new JButton("Search");
+		btnSearch_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String url = Constants.getURL(textField_1.getText(), textField_2.getText(), 1);
+				journeys = Parser.getJourneys( url );
+				ArrayList<Journey> journeysList = journeys.getJourneys();
+				Journey journey = journeysList.get(0);
+				
+				Calendar depTime = journey.getDepDateTime();
+				Calendar arrTime = journey.getArrDateTime();
+				
+				textArea_1.append("Byten: " + journey.getNoOfChanges() + "\n");
+				textArea_1.append("Börja med: " + journey.getLineTypeName());
+				textArea_1.append(": " + journey.getLineOnFirstJourney()+ "\n");
+				textArea_1.append("Antal zoner: " + journey.getNoOfZones() + "\n");
+				
+				textArea_1.append("Avgår från: " + journey.getStartStation());
+				textArea_1.append(" efter " + journey.getTimeToDeparture() + " signalfel\n");
+				textArea_1.append("Klockan: " + depTime.HOUR_OF_DAY + ":" + depTime.MINUTE);
+				textArea_1.append(" eventuell försening: " + journey.getDepTimeDeviation() + "\n");
+				textArea_1.append("Restid: " + journey.getTravelMinutes() + "min\n");
+				
+				textArea_1.append("Ankommer: " + arrTime.HOUR_OF_DAY + ":" + arrTime.MINUTE);
+				textArea_1.append(" till: " + journey.getEndStation() + "\n");
+			}
+		});
+		panel_3.add(btnSearch_1);
+		
+		JScrollPane scrollPane_1 = new JScrollPane();
+		panel_2.add(scrollPane_1, BorderLayout.CENTER);
+		
+		textArea_1 = new JTextArea();
+		scrollPane_1.setViewportView(textArea_1);
 		splitPane.setDividerLocation(280);
 	}
 
